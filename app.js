458980e84977197bbb549b4214de770ab6eeb2f8
Dao Ngoc Hien <11247288@st.neu.edu.vn>
2026-09-03 16:21:23 +0700

    Add

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
-let bedrooms = 3;
-const taxRate = 0.08;
+let form = document.querySelector("#score-form");
 
-if (bedrooms > 3) {
-    console.log("You have a large house.");
-    console.warn("Warning: Large houses may have higher maintenance costs.");
-    console.error("Error: Please ensure you have the necessary resources for a large house.");
-} else {
-    console.log("You have a small house.");
-}
-
-for (let i = 0; i <= bedrooms; i++) {
-    console.log('Bedroom ${i}');
-}
-
-let sample = { id: 1, name: "Sample Object", type: "Example" };
-
-console.log("Sample Object:", sample);
-console.log("Sample Object ID:", sample.id);
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
 
-let arr = [{ name: "Maths", score: 8 }, { name: "Programming", score: 9 }, { name: "English", score: 7 }];
-let sum = 0;
-let max = 0;
+    let mon1 = document.querySelector("#mon1").value;
+    let mon2 = document.querySelector("#mon2").value;
+    let mon3 = document.querySelector("#mon3").value;
+    console.log("mon1", mon1);
+    console.log("mon2", mon2);
+    console.log("mon3", mon3);
 
-for (let i = 0; i < arr.length; i++) {
-    sum += arr[i].score;
-    if (arr[i].score > 8) {
-        console.log("Subject with score > 8: " + arr[i].name);
-    }
-    if (arr[i].score > max) {
-        max = arr[i].score;
-    }
-}
+    let totalScore = parseFloat(mon1) + parseFloat(mon2) + parseFloat(mon3);
+    console.log("totalScore", totalScore);
 
-console.log("Sum of scores: " + sum);
-console.log("Maximum score: " + max);
+    let sum = document.querySelector("#sum");
+    sum.textContent = "Total Score: " + totalScore;
+
+});
